7a855b919e105169e0a3adea6237c4a95f7cfbea
Habibur Rahman <habibur88454@gmail.com>
2024-11-16 11:04:52 +0600

    active route added

--- src/pages/Home/MainContainer.jsx
+++ src/pages/Home/MainContainer.jsx
@@ -5,11 +5,11 @@ import RightSideNav from "./RightSideNav";
 const MainContainer = () => {
     return (
         <div className="grid md:grid-cols-12 gap-6">
-            <div className="col-span-3"><LeftSideNav></LeftSideNav></div>
-            <div className="col-span-6">
+            <div className="md:col-span-3"><LeftSideNav></LeftSideNav></div>
+            <div className="md:col-span-6">
                 <Outlet></Outlet>
             </div>
-            <div className="col-span-3"><RightSideNav></RightSideNav></div>
+            <div className="md:col-span-3"><RightSideNav></RightSideNav></div>
         </div>
     );
 };
